029e9aa2360865d543bb7e31e74ec9f3ccb897d9
lalitvasoya <lalit.vasoya@tntra.io>
2022-05-23 19:01:54 +0530

    [Add] chain operator.

--- src/components/students/Students.js
+++ src/components/students/Students.js
@@ -12,27 +12,27 @@ const Students = () => {
     });
     
     const studentOnChange = (e) => {
-        const newStudents= [...state.students];
-        const index = e.target.getAttribute('index');
-        newStudents[index]['name'] = e.target.value; 
+        const newStudents= [...state?.students];
+        const index = e?.target?.getAttribute('index');
+        newStudents[index]['name'] = e?.target?.value; 
         setState({...state, students: newStudents});
     }
 
     const students = state.students.map((ele, index)=>{
-        return <Student name={ele.name} city={ele.city} state={ele.state} index={index} key={index} studentOnChange={studentOnChange}/>
+        return <Student name={ele?.name} city={ele?.city} state={ele?.state} index={index} key={index} studentOnChange={studentOnChange}/>
     });
 
 
     const handleOnClick = () => {
-        console.log(state.show)
-        setState({...state, show: !state.show})
+        console.log(state?.show)
+        setState({...state, show: !state?.show})
     }
 
     return (
         <>
             <div>
-                <button onClick={handleOnClick}>{state.show? 'UnShow': 'show'}</button>
-                {state.show? students: null}
+                <button onClick={handleOnClick}>{state?.show? 'UnShow': 'show'}</button>
+                {state?.show? students: null}
             </div>
         </>
     ) 
